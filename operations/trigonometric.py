cf16761ba6c10434ca210914dc2381f18182c6a0
Kevin Gámez <est.kevin.gamez@unimilitar.edu.co>
2026-07-28 15:14:21 -0500

    Refactor: 'translated' functions to classes to obey `Operation` contract.

--- operations/trigonometric.py
+++ operations/trigonometric.py
@@ -1,33 +1,30 @@
 from math import sin, cos, tan, isclose, radians
+from core.operation import Operation
 from exceptions.calculator_error import UndefinedTangentError
 
-class Trigonometric():
-    """Class for trigonometric operations."""
-    
-    @staticmethod
-    def _degrees_to_radians(degrees):
-        """Convert degrees to radians."""
-        return radians(degrees)
-    
-    @staticmethod
-    def sin(x, unit='radians'):
-        """Calculate the sine of x (in radians)."""
+def _degrees_to_radians(degrees):
+    """Converts degrees to radians."""
+    return radians(degrees)
+
+class sine(Operation):
+    """Calculates the sine of a given angle."""
+    def execute(self, x, unit='radians'):
         if unit == 'degrees':
-            x = Trigonometric._degrees_to_radians(x)
+            x = _degrees_to_radians(x)
         return sin(x)
-    
-    @staticmethod
-    def cos(x, unit='radians'):
-        """Calculate the cosine of x (in radians)."""
+
+class cosine(Operation):
+    """Calculates the cosine of a given angle."""
+    def execute(self, x, unit='radians'):
         if unit == 'degrees':
-            x = Trigonometric._degrees_to_radians(x)
+            x = _degrees_to_radians(x)
         return cos(x)
 
-    @staticmethod
-    def tan(x, unit='radians'):
-        """Calculate the tangent of x (in radians)."""
+class tangent(Operation):
+    """Calculates the tangent of a given angle."""
+    def execute(self, x, unit='radians'):
         if unit == 'degrees':
-            x = Trigonometric._degrees_to_radians(x)
+            x = _degrees_to_radians(x)
         if isclose(cos(x), 0, abs_tol=1e-9):
-            raise UndefinedTangentError()
+            raise UndefinedTangentError
         return tan(x)
