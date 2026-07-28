859f348b92fad4aa2f85944b87c909865b9540d7
Kevin Gámez <est.kevin.gamez@unimilitar.edu.co>
2026-07-28 14:55:19 -0500

    Refactor: `Operation` isn't a 'wrapper' class anymore. Becomes a contract that every operation must comply with

--- core/operation.py
+++ core/operation.py
@@ -1,25 +1,7 @@
-from operations.arithmetic import Arithmetic
-from operations.trigonometric import Trigonometric
-from operations.taxes import Taxes
-from operations.discrete import Discrete
-from operations.exponential import Exponential
+from abc import ABC, abstractmethod
 
-class Operation:
-    """Class to 'wrap' all operations in the calculator."""
-    def __init__(self):
-        self.operations = {
-            'add'      : Arithmetic.add(),
-            'subtract' : Arithmetic.subtract(),
-            'multiply' : Arithmetic.multiply(),
-            'divide'   : Arithmetic.divide(),
-            'sin'      : Trigonometric.sin(),
-            'cos'      : Trigonometric.cos(),
-            'tan'      : Trigonometric.tan(),
-            'vat'      : Taxes.vat(),
-            'factorial': Discrete.factorial(),
-            'fibonacci': Discrete.fibonacci(),
-            'lcm'      : Discrete.lcm(),
-            'gcd'      : Discrete.gcd(),
-            'power'    : Exponential.power(),
-            'root'     : Exponential.root(),}
-        
+class Operation(ABC):
+
+    @abstractmethod
+    def execute(self, *args):
+        pass
