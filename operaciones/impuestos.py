9fe75e010970e150989ffb091e5ef9024e83174d
Shalon Leon <est.shalon.leon@unimilitar.edu.co>
2026-08-03 20:10:30 -0500

    Añadir operacion IVA

--- operaciones/impuestos.py
+++ operaciones/impuestos.py
@@ -1,1 +1,13 @@
 # Operaciones de Impuestos (Impuesto de Valor Agregado) - Shalon León
+from nucleo.operacion import Operacion
+class IVA(Operacion):
+    etiqueta = 'IVA'
+
+    def __init__(self, numero_a, numero_b):
+        self._numero_a = numero_a
+        self._numero_b = numero_b
+
+    def ejecutar(self):
+        multiplicacion = self._numero_a * self._numero_b
+        iva = multiplicacion / 100
+        return iva
