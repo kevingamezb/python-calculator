b33e365121b2895c8975eab3ac00565d1b09470b
Alvaro Orjuela <est.alvaroe.orjuela@unimilitar.edu.co>
2026-08-03 21:28:03 -0500

    trabajo terminado :)

--- operaciones/exponenciales.py
+++ operaciones/exponenciales.py
@@ -1,4 +1,29 @@
 # Operaciones Exponenciales (Potencia, Raíz) - Alvaro Orjuela
 
-def _es_par(numero):
-    return numero % 2 == 0
+#def _es_par(numero):
+
+
+
+
+from nucleo.operacion import Operacion
+
+class Potencia(Operacion):
+    etiqueta = 'Potencia'
+
+    def __init__(self, base, exponente):
+        self._base = base
+        self._exponente = exponente
+
+    def ejecutar(self):
+        return self._base ** self._exponente
+
+
+class Raiz(Operacion):
+    etiqueta = 'Raíz'
+
+    def __init__(self, radicando, indice):
+        self._radicando = radicando
+        self._indice = indice
+
+    def ejecutar(self):
+        return self._radicando ** (1/self._indice)
