023dba7e12cf331270cd3a98c8c1793123d87fe3
Andres Leon <est.andresf.leons@unimilitar.edu.co>
2026-08-03 21:07:23 -0500

    Añadir el resto de operaciones discretas.py (MCM, MCD, Fibonacci)

--- operaciones/discretas.py
+++ operaciones/discretas.py
@@ -1,7 +1,7 @@
 # Operaciones Discretas (Factorial, Fibonacci, Mínimo Común Múltiplo, Máximo Común Divisor) - Andrés León [Alvaro Orjuela (Factorial)]
 
 from nucleo.operacion import Operacion
-from excepciones.error_calculadora import ErrorFactorialNegativo
+from excepciones.error_calculadora import ErrorFactorialNegativo , ErrorFibonacciNegativo
 
 class Factorial(Operacion):
     etiqueta = 'Factorial'
@@ -15,3 +15,41 @@ class Factorial(Operacion):
         if self._numero_a == 0 or self._numero_a == 1:
             return 1
         return self._numero_a * Factorial(self._numero_a-1)
+    
+
+class MCM(Operacion):
+    etiqueta = "Minimo Comun Multiplo"
+
+    def __int__(self, numero_a, numero_b):
+        self._numero_a= numero_a
+        self._numero_b= numero_b
+
+    def ejecutar(self): 
+
+        while self._numero_b !=0: 
+            self._numero_a , self._numero_b = self._numero_b, self._numero_a % self._numero_b
+            return self._numero_a
+
+class Fibonacci(Operacion):
+    etiqueta = "Fibonacci"
+
+    def __init__(self, numero):
+        self._numero = numero
+
+    def ejecutar(self):
+        n = self._numero
+
+        if n<0: 
+            raise ErrorFibonacciNegativo
+
+        elif n==0:
+            return 0
+
+        elif n==1: 
+            return 1
+
+        a,b = 0,1
+
+        for i in range(1,n):
+            a,b = b, a+b
+            return b
