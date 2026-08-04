128689c515c0581156a1f661dc4b6dfcc7b72f80
Alvaro Orjuela <est.alvaroe.orjuela@unimilitar.edu.co>
2026-08-03 19:44:33 -0500

    añadir operacion factorial

--- operaciones/discretas.py
+++ operaciones/discretas.py
@@ -1,1 +1,18 @@
 # Operaciones Discretas (Factorial, Fibonacci, Mínimo Común Múltiplo, Máximo Común Divisor) - Andrés León [Alvaro Orjuela (Factorial)]
+
+from nucleo.operacion import Operacion
+from operaciones.discretas import Factorial
+from excepciones.error_calculadora import ErrorFactorialNegativo
+
+class Factioral(Operacion):
+    etiqueta = 'Factorial'
+
+    def __init__(self, numero_a):
+        self._numero_a = numero_a
+
+    def ejecutar(self):
+        if self._numero_a < 0:
+            raise ErrorFactorialNegativo
+        if self._numero_a == 0 or self._numero_a == 1:
+            return 1
+        return self._numero_a * Factorial(self._numero_a-1)
