767c74ad8c77e9183de5645c62dc577fd750cbce
Kevin Gámez <est.kevin.gamez@unimilitar.edu.co>
2026-08-03 19:54:38 -0500

    fix: corregir nombre de clase `Factorial`

--- operaciones/discretas.py
+++ operaciones/discretas.py
@@ -1,10 +1,9 @@
 # Operaciones Discretas (Factorial, Fibonacci, Mínimo Común Múltiplo, Máximo Común Divisor) - Andrés León [Alvaro Orjuela (Factorial)]
 
 from nucleo.operacion import Operacion
-from operaciones.discretas import Factorial
 from excepciones.error_calculadora import ErrorFactorialNegativo
 
-class Factioral(Operacion):
+class Factorial(Operacion):
     etiqueta = 'Factorial'
 
     def __init__(self, numero_a):
